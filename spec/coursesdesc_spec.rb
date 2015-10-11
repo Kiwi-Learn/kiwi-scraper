e47fb4ac29b5ba8420bf8c5ed1df1d019c96f151
vicky <lch82327@gmail.com>
2015-10-11 23:02:12 +0800

    add testing url data

--- spec/coursesdesc_spec.rb
+++ spec/coursesdesc_spec.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require '../lib/coursesdesc/courses.rb'
 require './courses_name_test_data.rb'
-# require './urls_test_data.rb'
+require './urls_test_data.rb'
 
 describe 'Courses desc scrap' do
   it 'should return an array include name of courses' do
@@ -15,6 +15,4 @@ describe 'Courses desc scrap' do
     courses_urls = sc.course_url
     courses_urls.must_equal URLS_LIST
   end
-
 end
-
