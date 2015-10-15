d7a0ec2ab474befbd795f4c39ebd9c9195408d00
John-Lin <linton.tw@gmail.com>
2015-10-15 15:53:35 +0800

    fix module loading fail

--- spec/coursesdesc_spec.rb
+++ spec/coursesdesc_spec.rb
@@ -1,18 +1,18 @@
 require 'minitest/autorun'
-require '../lib/coursesdesc/courses.rb'
-require './courses_name_test_data.rb'
-require './urls_test_data.rb'
+require './lib/coursesdesc.rb'
+require './spec/courses_name_test_data.rb'
+require './spec/urls_test_data.rb'
 
 describe 'Courses desc scrap' do
   it 'should return an array include name of courses' do
     sc = KiwiScraper::ShareCourse.new
     courses_names = sc.course_name
-    courses_names.must_equal COURSES_NAME_LIST
+    courses_names.to_set.must_equal COURSES_NAME_LIST.to_set
   end
 
   it 'should return an array include name of url' do
     sc = KiwiScraper::ShareCourse.new
     courses_urls = sc.course_url
-    courses_urls.must_equal URLS_LIST
+    courses_urls.to_set.must_equal URLS_LIST
   end
 end
