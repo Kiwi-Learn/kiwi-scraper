ba16c8e5826cf7d368eccc5795cc10c508f50b68
vicky <lch82327@gmail.com>
2015-10-15 17:58:01 +0800

    web playback stubbing

--- spec/coursesdesc_spec.rb
+++ spec/coursesdesc_spec.rb
@@ -1,18 +1,25 @@
 require 'minitest/autorun'
+require 'vcr'
+require 'webmock/minitest'
 require './lib/coursesdesc.rb'
 require './spec/courses_name_test_data.rb'
 require './spec/urls_test_data.rb'
 
-describe 'Courses desc scrap' do
-  it 'should return an array include name of courses' do
-    sc = KiwiScraper::ShareCourse.new
-    courses_names = sc.course_name
-    courses_names.sort.must_equal COURSES_NAME_LIST.sort
-  end
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+end
 
-  it 'should return an array include name of url' do
-    sc = KiwiScraper::ShareCourse.new
-    courses_urls = sc.course_url
-    courses_urls.sort.must_equal URLS_LIST.sort
+VCR.use_cassette('courses') do
+  sc = KiwiScraper::ShareCourse.new
+  describe 'Courses desc scrap' do
+    it 'should return an array include name of courses' do
+      courses_names = sc.course_name
+      courses_names.sort.must_equal COURSES_NAME_LIST.sort
+    end
+    it 'should return an array include name of url' do
+      courses_urls = sc.course_url
+      courses_urls.sort.must_equal URLS_LIST.sort
+    end
   end
 end
