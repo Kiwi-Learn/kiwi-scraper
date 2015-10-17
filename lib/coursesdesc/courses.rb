ebbe10b93c7c0b328c3ef417815c7dd7a998f5dd
System Administrator <root@necisamdeMacBook-Air.local>
2015-10-17 17:14:47 +0800

    add course_date

--- lib/coursesdesc/courses.rb
+++ lib/coursesdesc/courses.rb
@@ -18,7 +18,9 @@ module KiwiScraper
     def course_name
       @course_name ||= get_course_name
     end
-
+    def course_date
+      @course_date ||= get_course_date
+    end
     def course_url
       @course_url ||= get_course_url
     end
@@ -65,5 +67,16 @@ module KiwiScraper
 
       result
     end
+
+    def get_course_date
+      date = [] 
+      @document.xpath("//p[@id='courseDate']").each do |course|
+        longdate = course.text
+        date << longdate.split("：")[1]
+      end
+      File.write("dateoutput", date)
+      date
+    end
+
   end
 end
