d34b4417573249812d1fe62b63a6b7627fe468fd
vicky <lch82327@gmail.com>
2015-10-18 00:00:31 +0800

    courses.rb coding style fix

--- lib/coursesdesc/courses.rb
+++ lib/coursesdesc/courses.rb
@@ -8,32 +8,42 @@ module KiwiScraper
     URL = 'http://www.sharecourse.net/sharecourse/course/view/courseList'
 
     def initialize
-      get_html
-    end
-
-    def get_html
-      @document = Oga.parse_html(open(URL))
-    end
-
-    def course_name
-      @course_name ||= parse_course_name
-    end
-    def course_date
-      @course_date ||= get_course_date
-    end
-    def course_url
-      @course_url ||= parse_course_url
-    end
-
-    def courses_name_to_url_mapping
-      @course_map ||= parse_course
+      parse_html
     end
 
     def course_id
       @course_id ||= parse_course_id
     end
 
+    def course_name
+      @course_name ||= parse_course_name
+    end
+
+    def course_url
+      @course_url ||= parse_course_url
+    end
+
+    def course_date
+      @course_date ||= parse_course_date
+    end
+
+    def courses_name_to_url_mapping
+      @course_map ||= parse_course
+    end
+
     private
+
+    def parse_html
+      @document = Oga.parse_html(open(URL))
+    end
+
+    def parse_course_id
+      course_id = []
+      @document.xpath("//div[@class='gb_mid']").each do |course|
+        course_id << course.children[4].children[0].children.text.split('：')[1]
+      end
+      course_id
+    end
 
     def parse_course_name
       result = []
@@ -49,6 +59,16 @@ module KiwiScraper
         result << course.attributes[2].value.split("'")[1]
       end
       result
+    end
+
+    def parse_course_date
+      # date type yyyy-mm-dd - yyyy-mm-dd
+      date = []
+      @document.xpath("//p[@id='courseDate']").each do |course|
+        longdate = course.text
+        date << longdate.split('：')[1]
+      end
+      date
     end
 
     def parse_course
@@ -67,28 +87,7 @@ module KiwiScraper
         url << course.attributes[2].value.split("'")[1]
       end
 
-      result = Hash[hash_name.zip(url)]
-
-      result
+      Hash[hash_name.zip(url)] # return value
     end
-
-    def get_course_date
-      #date type yyyy-mm-dd - yyyy-mm-dd
-      date = []
-      @document.xpath("//p[@id='courseDate']").each do |course|
-        longdate = course.text
-        date << longdate.split("：")[1]
-      end
-      date
-    end
-
-    def parse_course_id
-      course_id = []
-      @document.xpath("//div[@class='gb_mid']").each do |course|
-        course_id << course.children[4].children[0].children.text.split("：")[1]
-      end
-      course_id
-    end
-
   end
 end
